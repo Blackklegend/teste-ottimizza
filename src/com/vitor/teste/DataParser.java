dcae7ce6c8f57c3e4211fcce0344d93132de4123
Blackk <73979580+Blackklegend@users.noreply.github.com>
2022-02-05 00:52:29 -0300

    Remade the DataParser

--- src/com/vitor/teste/DataParser.java
+++ src/com/vitor/teste/DataParser.java
@@ -1,62 +1,47 @@
 package com.vitor.teste;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DataParser {
     String data;
 
-    public DataParser(String data) {
-        char[] parts = data.toCharArray();
-        boolean allDigits = true, dateBR = false;
-        StringBuilder cleanData = new StringBuilder();
-        
-        for(int i = 0; i < parts.length; i++) {
-            if (Character.isDigit(parts[i])) {
-                cleanData.append(parts[i]);
-            }
-            else if (i == 4) { //referente à data yyyy-mm-dd
-                cleanData.append('-');
+    public DataParser(String data){
+        this.data = conversor(data);
+    }
+
+    private String parser(LocalDate dataToConvert){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
+        return dataToConvert.format(formatter);
+    }
+
+    private String conversor(String data) {
+        boolean allDigits = true;
+        LocalDate dataToConvert = null;
+        char[] dateToParse = data.toCharArray();
+
+        DateTimeFormatter   formatter1 = DateTimeFormatter.ofPattern("dd/MM/yyyy"),
+                            formatter2 = DateTimeFormatter.ofPattern("dd-MM-yyyy"),
+                            formatter3 = DateTimeFormatter.ofPattern("yyyy-MM-dd"),
+                            formatter4 = DateTimeFormatter.ofPattern("ddMMyyyy");
+
+
+        for(char c : dateToParse) {
+            if (!Character.isDigit(c)) {
                 allDigits = false;
             }
-            else if (i == 5) { //referente à data dd-mm-yyyy
-                cleanData.append('-');
-                dateBR = true;
-                allDigits = false;
-            }
         }
-        
-        // Caso a data inserida esteja em ddMMyyyy
-        if(allDigits) {
-            this.data = isAllDigits(cleanData);
-        }
-        
-        // Caso a data inserida esteja em dd-MM-yyyy
-        if(dateBR) {
-            this.data = isBR(cleanData);
-        }
-        
-        // Caso a data inserida esteja em yyyy-mm-dd
-        if(cleanData.length() > 7){
-            cleanData.delete(7,9);
-            this.data = cleanData.toString();
-        }
-        
-    }
-    
-    //Organiza uma data composta por digitos no formato ddMMyyyy
-    private String isAllDigits(StringBuilder cleanData) {
-        cleanData.delete(0, 2);
-        String newData = cleanData.substring(0, 2);
-        cleanData.append(newData);
-        cleanData.delete(0,2);
-        cleanData.insert(4, '-');
-        return cleanData.toString();
-    }
-    //Organiza uma data formato dd/mm/yyyy
-    private String isBR(StringBuilder cleanData) {
-        cleanData.delete(0, 2);
-        String newData = cleanData.substring(0, 2);
-        cleanData.append(newData);
-        cleanData.delete(0,3);
-        cleanData.insert(4, '-');
-        return cleanData.toString();
+
+        if (allDigits)
+            dataToConvert = LocalDate.parse(data, formatter4);
+        else if (data.indexOf("-") > 3)
+            dataToConvert = LocalDate.parse(data, formatter3);
+        else if(data.contains("-"))
+            dataToConvert = LocalDate.parse(data, formatter2);
+        else if(data.contains("/"))
+            dataToConvert = LocalDate.parse(data, formatter1);
+
+        assert dataToConvert != null;
+        return parser(dataToConvert);
     }
 }
